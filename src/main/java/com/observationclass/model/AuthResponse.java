56a1d6ab6fadd7d043be966fd20e0ad465b031f2
ngocquang <ngocquang2000fpt@gmail.com>
2022-10-31 16:25:10 +0700

    update login

--- src/main/java/com/observationclass/model/AuthResponse.java
+++ src/main/java/com/observationclass/model/AuthResponse.java
@@ -12,12 +12,14 @@ public class AuthResponse {
     private String userName;
     private String accessToken;
     private String tokenType;
+    private Integer campusId;
     private Set<Role> setRole;
 
-    public AuthResponse(String userName, String accessToken, Set<Role> setRole) {
+    public AuthResponse(String userName, String accessToken,Integer campusId, Set<Role> setRole) {
         this.userName = userName;
         this.accessToken = accessToken;
         this.tokenType="Bearer";
+        this.campusId=campusId;
         this.setRole = setRole;
     }
 }
